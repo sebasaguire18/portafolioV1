46a3cf75180665d454d744ab0d1798d72366cafb
Sebastian Aguirre <sebasavmt@gmail.com>
2020-04-29 22:24:48 -0500

    integrando los productos con fireBase

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from './../interfaces/producto.interface';
+
+
+@
+Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  public cargando = true;
+  public producto:Producto[];
+
+
+  constructor(private http : HttpClient ) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos(){
+    this.http.get('https://portafolio-a42dd.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: Producto[]) => {
+
+      this.producto = resp;
+      
+      setTimeout(() => {
+        this.cargando = false;
+      },300);
+
+    });
+
+
+
+  }
+}
